9f06c7c4dc119850ffa41f5642100c3210ec7ec8
Ionică Bizău <bizauionica@gmail.com>
2016-03-23 12:50:28 +0200

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,42 @@
+(function () {
+
+    var $result = document.getElementById("result");
+
+    function update () {
+        var result = null
+          , input = editor.getValue()
+          ;
+
+        try {
+            result = ejs.render(input)
+            $result.parentNode.style.background = "#27ae60";
+        } catch (e) {
+            result = e.stack;
+            $result.parentNode.style.background = "#c0392b";
+        }
+
+        $result.textContent = result;
+    }
+
+    var editor = ace.edit("editor");
+    editor.setTheme("ace/theme/monokai");
+    editor.getSession().setMode("ace/mode/ejs");
+    editor.on("change", update);
+    editor.setValue(`OK, so have fun! :D
+-------------------
+<%
+    var fruits = ["Apple", "Pear", "Orage", "Lemon"]
+      , random = " ".repeat(198).split("").map(x => Math.random())
+      ;
+%>
+
+These fruits are amazing:
+<% for(var i = 0; i < fruits.length; ++i) {%>
+  - <%=fruits[i]%>s<% } %>
+
+Let's see some random numbers:
+
+<% random.forEach((c, i) => {
+%> <%=c.toFixed(10) + ((i + 1) % 6 === 0 ? "\\n": "") %><%});%>`, -1);
+    editor.focus();
+})();
